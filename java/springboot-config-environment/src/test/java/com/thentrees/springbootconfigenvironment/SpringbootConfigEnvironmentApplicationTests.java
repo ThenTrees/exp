b31823bc20ae9490521ab7a10b270381879314b7
Tran Le Thien Tri <thientri.trank17@gmail.com>
2024-12-17 15:07:26 +0700

    spring-profile

---
+++ java/springboot-config-environment/src/test/java/com/thentrees/springbootconfigenvironment/SpringbootConfigEnvironmentApplicationTests.java
@@ -0,0 +1,13 @@
+package com.thentrees.springbootconfigenvironment;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringbootConfigEnvironmentApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
